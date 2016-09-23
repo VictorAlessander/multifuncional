88ad60b448e4ee727bc0f3b34720ff0d878ea4af
Victor Alessander <VictorAlessander@users.noreply.github.com>
2016-09-23 00:58:08 -0300

    Add files via upload

---
+++ funcoes.h
@@ -0,0 +1,56 @@
+
+void menu(){
+
+    printf("\nOpções disponíveis\n"
+    "+--------------------------+\n"
+    "|[1] Múltiplo de cinco     |\n"
+    "|[2] Par ou ímpar          |\n"
+    "|[3] Valor ao cubo         |\n"
+    "|[4] Sucessor e antecessor |\n"
+    "|[5] Calculadora           |\n"
+    "|[0] Sair                  |\n"
+    "+--------------------------+\n");
+}
+
+int mult_cinco(int a){
+    if(a % 5 == 0){
+        return "É múltiplo de cinco";
+    }
+    else{
+        return "Não é múltiplo de cinco";
+    }
+}
+
+int sucessor_antecessor(int a){
+    return a;
+}
+
+int cubo(int a){
+    return pow(a, 3);
+}
+
+int par_impar(int a){
+    if(a % 2 == 0){
+        return "Valor par";
+    }
+    else{
+        return "Valor ímpar";
+    }
+
+}
+
+float soma(float a, float b){
+    return a + b;
+}
+
+float subtracao(float a, float b){
+    return a - b;
+}
+
+float multiplicacao(float a, float b){
+    return a * b;
+}
+
+float divisao(float a, float b){
+    return a / b;
+}
---
+++ multifuncional.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#include "funcoes.h" /* Arquivo com as funções criadas para o programa */
+
+/*
+ * FUNCIONALIDADES DO PROGRAMA
+ * ---------------------------
+ * 1- SUCESSOR E ANTECESSOR
+ * 2- VALOR MÚLTIPLO DE CINCO
+ * 3- VALOR AO CUBO
+ * 4- CALCULADORA BÁSICA
+ * 5- NÚMERO PAR OU ÍMPAR
+ */
+
+int main(){
+    float num1, num2; /* Calculadora */
+    int operacao; /* Calculadora */
+    int valor1; /* Operações com inteiros */
+    int opcao; /* Opções do menu */
+
+    setlocale(LC_ALL,"");
+
+    printf("\t\t\tBem vindo!\n");
+
+    do{
+        menu();
+        scanf("%d", &opcao);
+
+        switch(opcao){
+        case 0:
+            printf("[*] Programa finalizado\n");
+            break;
+
+        case 1:
+            printf("\nInsira o valor: ");
+            scanf("%d", &valor1);
+            printf("%s", mult_cinco(valor1));
+            break;
+
+        case 2:
+            printf("\nValor a ser avaliado: ");
+            scanf("%d", &valor1);
+            printf("%s", par_impar(valor1));
+            break;
+
+        case 3:
+            printf("\nInsira o valor: ");
+            scanf("%d", &valor1);
+            printf("%d ao cubo = %d", valor1, cubo(valor1));
+            break;
+
+        case 4:
+            printf("\nValor: ");
+            scanf("%d", &valor1);
+            printf("Sucessor: %d / Antecessor: %d", sucessor_antecessor(valor1) + 1, sucessor_antecessor(valor1) - 1);
+            break;
+
+        default:
+            printf("[!] Opção inválida");
+            break;
+
+        case 5:
+            printf("Primeiro valor: ");
+            scanf("%f", &num1);
+
+            printf("Segundo valor: ");
+            scanf("%f", &num2);
+
+            printf("Operações disponíveis:\nSoma [1] / Subtração [2] / Multiplicação [3] / Divisão [4]\n>> ");
+            scanf("%d", &operacao);
+
+            switch(operacao){
+
+            case 1:
+                printf("\nResultado: %.2f", soma(num1, num2));
+                break;
+
+            case 2:
+                printf("\nResultado: %.2f", subtracao(num1, num2));
+                break;
+
+            case 3:
+                printf("\nResultado: %.2f", multiplicacao(num1, num2));
+                break;
+
+            case 4:
+                printf("\nResultado: %.2f", divisao(num1, num2));
+                break;
+
+            default:
+                printf("\n[!] Operação inválida");
+                break;
+            }
+        }
+
+    }while(opcao != 0);
+}
