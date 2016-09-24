fc5764125eb92de875c960b8913ca62f895ee5d8
Victor <alessander@protonmail.com>
2016-09-24 02:17:34 -0300

    Adição e modificação de funções

--- funcoes.h
+++ funcoes.h
@@ -1,5 +1,111 @@
+#include <string.h>
+#include <stdlib.h>
 
-void menu(){
+void limpar_buffer(){
+    #ifdef __linux__
+        __fpurge(stdin);
+    #endif // __linux__
+
+    #ifdef __WIN32__
+        fflush(stdin);
+    #endif // __WIN32__
+}
+
+void tipo(){
+    printf("+-----------------------------+\n|ALUNO = [1] / PROFESSOR = [2]|\n+-----------------------------+\n=> ");
+}
+
+char area_aluno(char login[30], char password[30]){
+    int erro = 0;
+
+    /* Enquanto erro for menor ou igual a 3 */
+    while(erro <= 3){
+        printf("User: ");
+        fgets(login, 30, stdin);
+        printf("Password: ");
+        fgets(password, 30, stdin);
+
+        /* Se tamanho do login e password digitados for igual ou superior a 29 caracteres, o programa sofre BREAK */
+        if(strlen(login) >= 29 || strlen(password) >= 29){
+            printf("[!] Login or password not expected\n");
+            break;
+        }
+        else{
+            printf("-------------------------------------------\n");
+            printf("Login written: %s", login);
+            printf("\nPassword written: %s\n", password);
+            printf("-------------------------------------------\n");
+
+            /* Compara se login é igual a "root" e se password é igual a "toor". Se sim, imprime mensagem de sucesso e sofre BREAK */
+            if(strcmp(login, "root\n") == 0 && strcmp(password, "toor\n") == 0){
+                printf("[*] Login sucessful\n");
+                system("cls");
+                printf("[*] Bem vindo, Victor!\n");
+                break;
+            }
+                /* Se não acontecer, irá adicionar +1 a variável erro */
+            else{
+                erro++;
+                /* Se erro for menor ou igual a 3, imprime mensagem de senha incorreta */
+                if(erro < 3){
+                    printf("[!] Login or password is wrong\n");
+                }
+                /* Se erro for maior que 3, o programa imprime mensagem e sofre BREAK */
+                else if(erro >= 3){
+                    printf("Incorrect for 3 times\nBye\n");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+char area_professor(char login[30], char password[30]){
+    int erro = 0;
+
+    /* Enquanto erro for menor ou igual a 3 */
+    while(erro <= 3){
+        printf("User: ");
+        fgets(login, 30, stdin);
+        printf("Password: ");
+        fgets(password, 30, stdin);
+
+        /* Se tamanho do login e password digitados for igual ou superior a 29 caracteres, o programa sofre BREAK */
+        if(strlen(login) >= 29 || strlen(password) >= 29){
+            printf("[!] Login or password not expected\n");
+            break;
+        }
+        else{
+            printf("-------------------------------------------\n");
+            printf("Login written: %s", login);
+            printf("\nPassword written: %s\n", password);
+            printf("-------------------------------------------\n");
+
+            /* Compara se login é igual a "root" e se password é igual a "toor". Se sim, imprime mensagem de sucesso e sofre BREAK */
+            if(strcmp(login, "joao\n") == 0 && strcmp(password, "bosco\n") == 0){
+                printf("[*] Login sucessful\n");
+                system("cls");
+                printf("[*] Bem vindo, João!\n");
+                break;
+            }
+                /* Se não acontecer, irá adicionar +1 a variável erro */
+            else{
+                erro++;
+                /* Se erro for menor ou igual a 3, imprime mensagem de senha incorreta */
+                if(erro < 3){
+                    printf("[!] Login or password is wrong\n");
+                }
+                /* Se erro for maior que 3, o programa imprime mensagem e sofre BREAK */
+                else if(erro >= 3){
+                    printf("Incorrect for 3 times\nBye\n");
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void menu_alunos(){
 
     printf("\nOpções disponíveis\n"
     "+--------------------------+\n"
@@ -8,6 +114,20 @@ void menu(){
     "|[3] Valor ao cubo         |\n"
     "|[4] Sucessor e antecessor |\n"
     "|[5] Calculadora           |\n"
+    "|[0] Sair                  |\n"
+    "+--------------------------+\n");
+}
+
+void menu_professor(){
+
+    printf("\nOpções disponíveis\n"
+    "+--------------------------+\n"
+    "|[1] Múltiplo de cinco     |\n"
+    "|[2] Par ou ímpar          |\n"
+    "|[3] Valor ao cubo         |\n"
+    "|[4] Sucessor e antecessor |\n"
+    "|[5] Calculadora           |\n"
+    "|[6] Situação dos Alunos   |\n"
     "|[0] Sair                  |\n"
     "+--------------------------+\n");
 }
@@ -53,4 +173,52 @@ float multiplicacao(float a, float b){
 
 float divisao(float a, float b){
     return a / b;
+}
+
+float notas_alunos(){
+    char nomes[6][30], situacao[6][10];
+    float prova1[6], prova2[6], media[6];
+    int x;
+
+    /* Insere os nomes na matriz nomes */
+    for(x = 0; x < 6; x++){
+        printf("Aluno: ");
+        fflush(stdin);
+        fgets(nomes[x], 30, stdin);
+    }
+
+    /* Insere os valores na matriz prova1 */
+    for(x = 0; x < 6; x++){
+        printf("Elemento [%d]: ", x);
+        scanf("%f", &prova1[x]);
+    }
+
+    /* Insere os valores na matriz prova2 */
+    for(x = 0; x < 6; x++){
+        printf("Elemento [%d]: ", x);
+        scanf("%f", &prova2[x]);
+    }
+
+    /* Insere a soma da matriz prova1 + prova2 na matriz media */
+    for(x = 0; x < 6; x++){
+        media[x] = (prova1[x] + prova2[x]) / 2;
+    }
+
+    /* Insere a situação do aluno no vetor situacao */
+    for(x = 0; x < 6; x++){
+        if(media[x] > 5){
+            strcpy(situacao[x], "Aprovado");
+        }
+        else{
+            strcpy(situacao[x], "Reprovado");
+        }
+    }
+
+    system("cls");
+    /* Exibe a situação do aluno por intermédio do vetor situação */
+    for(x = 0; x < 6; x++){
+        printf("Aluno: %s\n", nomes[x]);
+        printf("Situacao: %s\n", situacao[x]);
+        printf("--------------------\n");
+    }
 }
--- multifuncional.c
+++ multifuncional.c
@@ -2,6 +2,8 @@
 #include <math.h>
 #include <locale.h>
 #include "funcoes.h" /* Arquivo com as funções criadas para o programa */
+#include <string.h>
+#include <stdlib.h>
 
 /*
  * FUNCIONALIDADES DO PROGRAMA
@@ -13,19 +15,14 @@
  * 5- NÚMERO PAR OU ÍMPAR
  */
 
-int main(){
-    float num1, num2; /* Calculadora */
-    int operacao; /* Calculadora */
-    int valor1; /* Operações com inteiros */
-    int opcao; /* Opções do menu */
-
-    setlocale(LC_ALL,"");
-
-    printf("\t\t\tBem vindo!\n");
+float funcionalidades_aluno(){
+    float num1, num2;
+    int valor1, opcao, operacao;
 
     do{
-        menu();
+        menu_alunos();
         scanf("%d", &opcao);
+        limpar_buffer();
 
         switch(opcao){
         case 0:
@@ -95,4 +92,110 @@ int main(){
         }
 
     }while(opcao != 0);
+}
+
+float funcionalidades_professor(){
+    int valor1, opcao, operacao;
+    float num1, num2;
+
+    do{
+        menu_professor();
+        scanf("%d", &opcao);
+        limpar_buffer();
+
+        switch(opcao){
+        case 0:
+            printf("[*] Programa finalizado\n");
+            break;
+
+        case 1:
+            printf("\nInsira o valor: ");
+            scanf("%d", &valor1);
+            printf("%s", mult_cinco(valor1));
+            break;
+
+        case 2:
+            printf("\nValor a ser avaliado: ");
+            scanf("%d", &valor1);
+            printf("%s", par_impar(valor1));
+            break;
+
+        case 3:
+            printf("\nInsira o valor: ");
+            scanf("%d", &valor1);
+            printf("%d ao cubo = %d", valor1, cubo(valor1));
+            break;
+
+        case 4:
+            printf("\nValor: ");
+            scanf("%d", &valor1);
+            printf("Sucessor: %d / Antecessor: %d", sucessor_antecessor(valor1) + 1, sucessor_antecessor(valor1) - 1);
+            break;
+
+        case 6:
+            notas_alunos();
+            break;
+
+        default:
+            printf("[!] Opção inválida");
+            break;
+
+        case 5:
+            printf("Primeiro valor: ");
+            scanf("%f", &num1);
+
+            printf("Segundo valor: ");
+            scanf("%f", &num2);
+
+            printf("Operações disponíveis:\nSoma [1] / Subtração [2] / Multiplicação [3] / Divisão [4]\n>> ");
+            scanf("%d", &operacao);
+
+            switch(operacao){
+
+            case 1:
+                printf("\nResultado: %.2f", soma(num1, num2));
+                break;
+
+            case 2:
+                printf("\nResultado: %.2f", subtracao(num1, num2));
+                break;
+
+            case 3:
+                printf("\nResultado: %.2f", multiplicacao(num1, num2));
+                break;
+
+            case 4:
+                printf("\nResultado: %.2f", divisao(num1, num2));
+                break;
+
+            default:
+                printf("\n[!] Operação inválida");
+                break;
+            }
+        }
+
+    }while(opcao != 0);
+}
+
+int main(){
+
+    int area; /* Aluno ou professor*/
+    char login[30], password[30]; /* Vetores para autenticação */
+
+    setlocale(LC_ALL,"");
+
+    printf("\t\t\tBem vindo!\n");
+
+    tipo();
+    scanf("%d", &area);
+    limpar_buffer();
+
+    if(area == 1){
+        area_aluno(login, password);
+        funcionalidades_aluno();
+    }
+    else if(area == 2){
+        area_professor(login, password);
+        funcionalidades_professor();
+    }
 }
